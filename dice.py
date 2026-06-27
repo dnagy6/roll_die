8e476a7dd5157eac6abd74eb1240f4cbfd733511
Dakota Nagy <dnagy6@student.cscc.edu>
2026-06-27 12:30:29 -0400

    Setup Die class using __init__ and roll_die

--- dice.py
+++ dice.py
@@ -11,3 +11,10 @@ Date: 07/28/2026
 
 """
 from random import randint
+
+class Die:
+    def __init__(self, sides=6):
+        self.sides = sides
+
+    def roll_die(self):
+        print(randint(1, self.sides))
