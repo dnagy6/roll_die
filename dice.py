ea867d77b878fd4a974540d28c2429d3761c2a75
Dakota Nagy <dnagy6@student.cscc.edu>
2026-06-27 12:17:12 -0400

    Added Docstring Header

---
+++ dice.py
@@ -0,0 +1,12 @@
+"""
+Participation Activity 2: Roll_Dice
+
+Description:
+Make a class Die with one attribute filled sides, which has a default value 
+of 6. Write a method called roll_die() that prints a random number between 1 and the 
+number of sides the die has. Make a 6-sided die and roll it 10 times.
+
+Author: Dakota Nagy
+Date: 07/28/2026
+
+"""
